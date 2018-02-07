d643352ba9bf7fab3626a3ae3210a00aab160a98
NeverMore47 <18607122978@163.com>
2018-02-07 17:33:00 +0800

    登录完善

--- web/src/main/java/com/google/controller/UserController.java
+++ web/src/main/java/com/google/controller/UserController.java
@@ -9,6 +9,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.Objects;
+
 /**
  * Created by wanjiahuan on 2018/2/6.
  * Description
@@ -22,12 +25,21 @@ public class UserController {
 
     @RequestMapping("/login")
     @ResponseBody
-    public Result login() {
-        UserDTO userDTO = new UserDTO();
-        userDTO.setUserName("admin");
-        userDTO.setUserPwd("123456");
+    public Result login(HttpServletRequest request) {
 
-        Result result = userService.login(userDTO);
+        String userName = request.getParameter("userName");
+        String password = request.getParameter("password");
+
+        Result result = new Result();
+        if (!Objects.isNull(userName) && !Objects.isNull(password)) {
+            UserDTO userDTO = new UserDTO();
+            userDTO.setUserName("admin");
+            userDTO.setUserPwd("123456");
+
+            result = userService.login(userDTO);
+        } else {
+            result.setSuccessAndMessage(false, "参数错误");
+        }
 
         return result;
     }
