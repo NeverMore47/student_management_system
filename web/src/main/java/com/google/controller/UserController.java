8f98f1a05ae460df9745785566e522ef9a0acbef
NeverMore47 <18607122978@163.com>
2018-02-08 18:35:13 +0800

    更改请求

--- web/src/main/java/com/google/controller/UserController.java
+++ web/src/main/java/com/google/controller/UserController.java
@@ -8,6 +8,7 @@ import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
@@ -24,7 +25,7 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    @RequestMapping("/login")
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
     @ResponseBody
     public Result login(UserDTO userDTO) {
 
